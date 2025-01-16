e46a7b79a39e5936f3ffb5d6fa98271b2c89910b
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-16 09:38:19 -0300

    feat(forgetPassword component): creating forgetPaswword component to authentication page

---
+++ src/components/AuthenticationPage/ForgetPassword.jsx
@@ -0,0 +1,8 @@
+export default function ForgetPassword({ title, subtitle }) {
+    return(
+        <div>
+            <h1 className='text-2xl font-bold'>{title}</h1>
+            <p className="text-xs">{subtitle}</p>
+        </div>
+    )
+}
