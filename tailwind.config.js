0707cc6c9dc8c669608d1e91f559e996dc125fd4
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-16 09:44:04 -0300

    chore(background): adding login background url

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login-background': "url('src/assets/login-background.jpg')",
+      }
+    },
   },
   plugins: [],
 }
