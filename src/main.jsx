e97425135b158fb8ec14d3f073e0b3a3cbc284dd
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-17 20:51:47 -0300

    feat(main): creating routes, importing pages and exporting the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,57 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import Layout from './layout/Layout.jsx'
+import AuthLayout from './layout/AuthLayout.jsx'
+
+import DashboardPage from './pages/Dashboard.jsx'
+import ClientsPage from './pages/Clients.jsx'
+import OrdersPage from './pages/Orders.jsx'
+
+import Authentication from './pages/Authentication.jsx'
+
 import App from './App.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <AuthLayout />,
+        children: [
+          {
+            path: '',
+            element: <Authentication />,
+          },
+        ],
+      },
+      {
+        path: '/',
+        element: <Layout />,
+        children: [
+          {
+            path: 'dashboard',
+            element: <DashboardPage />,
+          },
+          {
+            path: 'orders',
+            element: <OrdersPage />,
+          },
+          {
+            path: 'clients',
+            element: <ClientsPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
