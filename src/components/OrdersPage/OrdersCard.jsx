a04f6385286c32eb2de036d4bc42fbea6abaeb50
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-29 16:41:26 -0300

    feat: importing orders expand modal to component

--- src/components/OrdersPage/OrdersCard.jsx
+++ src/components/OrdersPage/OrdersCard.jsx
@@ -4,20 +4,31 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
+import OrdersExpandModal from './OrdersExpandModal'
+
 export default function BasicCard() {
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false)
+
     return (
-        <Card>
-            <CardActionArea>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        Nome do cliente
-                    </Typography>
-                    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>data do pedido</Typography>
-                    <Typography variant="body2">
-                        Valor total do pedido:
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <section>
+            <Card onClick={openModal}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            Nome do cliente
+                        </Typography>
+                        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>data do pedido</Typography>
+                        <Typography variant="body2">
+                            Valor total do pedido:
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+
+            <OrdersExpandModal isOpen={isModalOpen} onClose={closeModal} />
+        </section>
     );
 }
