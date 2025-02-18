f4a000a2c93aa46ec737ea61fa400c6869159883
Lucas Gaspari <lcsgaspari@gmail.com>
2025-02-18 14:19:05 -0300

    feat: adding protection to unregistered users to dashboard routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
-import Layout from './layout/Layout.jsx'
-import AuthLayout from './layout/AuthLayout.jsx'
+import Layout from './layout/Layout.jsx';
+import AuthLayout from './layout/AuthLayout.jsx';
 
-import DashboardPage from './pages/Dashboard.jsx'
-import ClientsPage from './pages/Clients.jsx'
-import OrdersPage from './pages/Orders.jsx'
+import DashboardPage from './pages/Dashboard.jsx';
+import ClientsPage from './pages/Clients.jsx';
+import OrdersPage from './pages/Orders.jsx';
 
-import Authentication from './pages/Authentication.jsx'
+import Authentication from './pages/Authentication.jsx';
 
-import App from './App.jsx'
-import './index.css'
+import { ToastContainer } from 'react-toastify';
+
+import App from './App.jsx';
+import './index.css';
+
+import { AuthProvider } from './auth/AuthContext.jsx'; // Importe o AuthProvider
+import ProtectedRoute from './auth/ProtectedRoute'; // Importe o ProtectedRoute
 
 const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <AuthLayout />,
+    {
+        element: <App />,
         children: [
-          {
-            path: '',
-            element: <Authentication />,
-          },
+            {
+                path: '/',
+                element: <AuthLayout />,
+                children: [
+                    {
+                        path: '',
+                        element: <Authentication />,
+                    },
+                ],
+            },
+            {
+                path: '/',
+                element: <ProtectedRoute><Layout /></ProtectedRoute>,
+                children: [
+                    {
+                        path: 'dashboard',
+                        element: (
+                            <DashboardPage />
+                        ),
+                    },
+                    {
+                        path: 'orders',
+                        element: (
+                            <OrdersPage />
+                        ),
+                    },
+                    {
+                        path: 'clients',
+                        element: (
+                            <ClientsPage />
+                        ),
+                    },
+                ],
+            },
         ],
-      },
-      {
-        path: '/',
-        element: <Layout />,
-        children: [
-          {
-            path: 'dashboard',
-            element: <DashboardPage />,
-          },
-          {
-            path: 'orders',
-            element: <OrdersPage />,
-          },
-          {
-            path: 'clients',
-            element: <ClientsPage />,
-          },
-        ],
-      },
-    ],
-  },
+    },
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <StrictMode>
+        <AuthProvider> {/* Envolva o RouterProvider com o AuthProvider */}
+            <ToastContainer />
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </StrictMode>,
+);
