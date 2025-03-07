50c3839640a277804d4b6435954f51a05fde9c6d
Lucas Gaspari <lcsgaspari@gmail.com>
2025-03-07 15:30:34 -0300

    feat: adding searchTerm parameter to ClientButtons

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import ClientList from '../components/ClientsPage/ClientList.jsx'
 import ClientButtons from '../components/ClientsPage/ClientButtons.jsx'
 import { ClientProvider } from '../context/ClientContext.jsx'; // Importe o ClientProvider
 
 export default function Clients() {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <>
             <ClientProvider>
-                <ClientButtons />
-                <ClientList />
+                <ClientButtons setSearchTerm={setSearchTerm} />
+                <ClientList searchTerm={searchTerm} />
             </ClientProvider>
         </>
     )
